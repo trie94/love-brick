9c8f722a9b4940e2c7ace714c4b5cda0a71b8708
yein <trie60@gmail.com>
2018-10-23 21:06:41 -0400

    add mk glow shader
    add mk glow shader

--- Assets/Scripts/BlockBehavior.cs
+++ Assets/Scripts/BlockBehavior.cs
@@ -6,12 +6,16 @@ public class BlockBehavior : MonoBehaviour
 {
     public bool isHit { get; set; }
     MeshRenderer meshRenderer;
+	Renderer renderer;
     Color originalColor;
+    [SerializeField] float lerpFactor = 1f;
 
     private void Awake()
     {
         meshRenderer = GetComponent<MeshRenderer>();
+		renderer = GetComponent<Renderer>();
         originalColor = meshRenderer.material.color;
+		StartCoroutine(Glow());
     }
 
     private void Update()
@@ -20,13 +24,48 @@ public class BlockBehavior : MonoBehaviour
         {
             // hover effect
             meshRenderer.material.color = Color.red;
-			Debug.Log(this.gameObject.name + "is hit");
+            Debug.Log(this.gameObject.name + "is hit");
         }
         else if (meshRenderer.material.color != originalColor)
         {
             // static
             meshRenderer.material.color = originalColor;
-			Debug.Log(this.gameObject.name + "is no more hit");
+            Debug.Log(this.gameObject.name + "is no more hit");
+        }
+    }
+
+    private IEnumerator Glow()
+    {
+		float lerpTime = 0f;
+        float glowPower = 0f;
+		float minGlowPower = 0f;
+		float maxGlowPower = 1f;
+
+		// float glowTexPower = 0f;
+		// float minGlowTexPower = 0f;
+		// float maxGlowTexPower = 5f;
+
+		// this should be changed
+        while (true)
+        {
+			lerpTime += lerpFactor * Time.deltaTime;
+            glowPower = Mathf.Lerp(minGlowPower, maxGlowPower, lerpTime);
+            meshRenderer.material.SetFloat("_MKGlowPower", glowPower);
+            // meshRenderer.material.SetFloat("_MKGlowTexStrength", glowTexPower);
+
+            if (lerpTime >= 1f)
+            {
+				float temp = minGlowPower;
+				minGlowPower = maxGlowPower;
+				maxGlowPower = temp;
+
+				// float temp2 = minGlowTexPower;
+				// minGlowPower = maxGlowTexPower;
+				// maxGlowTexPower = temp2;
+				lerpTime = 0f;
+            }
+
+            yield return null;
         }
     }
 }
